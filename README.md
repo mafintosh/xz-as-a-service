66afb29b97c05cc2db1e5142191949e81220a58c
Mathias Buus <mathiasbuus@gmail.com>
2017-11-22 16:00:47 +0100

    respond to mdns

--- README.md
+++ README.md
@@ -20,6 +20,18 @@ Then send a file to it you want to compress
 curl --upload-file my.tar http://localhost:9927/ > my.tar.xz
 ```
 
+The service will also announce itself as `xz-as-a-service.local` so you can do
+
+```sh
+curl --upload-file my.tar http://xz-as-a-service.local:9927 > my.tar.xz
+```
+
+If you want to change the name there is a `--name` flag
+
+```sh
+xz-as-a-service --name another-name --port 9927
+```
+
 ## License
 
 MIT
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
+var os = require('os')
 var http = require('http')
 var proc = require('child_process')
 var pump = require('pump')
+var mdns = require('multicast-dns')
 var argv = require('minimist')(process.argv, {
   alias: {
-    port: 'p'
+    port: 'p',
+    name: 'n'
   },
   default: {
-    port: process.env.PORT || 9927
+    port: process.env.PORT || 9927,
+    name: 'xz-as-a-service'
   }
 })
 
@@ -21,4 +25,41 @@ var server = http.createServer(function (req, res) {
 
 server.listen(argv.port, function () {
   console.log(`xz-as-a-service listening on ${server.address().port}`)
+
+  if (argv.name === false) return
+
+  var dns = mdns()
+  var name = argv.name + '.local'
+
+  dns.on('query', function (query) {
+    var qs = query.questions
+    for (var i = 0; i < qs.length; i++) {
+      var q = qs[i]
+      if (q.name === name && (q.type === 'A' || q.type === 'AAAA')) {
+        dns.respond({
+          answers: answers()
+        })
+      }
+    }
+
+    function answers () {
+      var ans = []
+      var networks = os.networkInterfaces()
+
+      Object.keys(networks).forEach(function (net) {
+        var n = networks[net]
+        n.forEach(function (addr) {
+          if (!addr.internal) {
+            ans.push({type: addr.family === 'IPv4' ? 'A' : 'AAAA', ttl: 120, name: name, data: addr.address})
+          }
+        })
+      })
+
+      if (!ans.length) {
+        ans.push({type: 'A', ttl: 120, name: name, data: '127.0.0.1'})
+        ans.push({type: 'AAAA', ttl: 120, name: name, data: '::1'})
+      }
+      return ans
+    }
+  })
 })
